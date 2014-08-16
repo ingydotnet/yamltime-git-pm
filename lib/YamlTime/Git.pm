8e822953f8bc58e5ad69fa6047ae5095f0a14b4a
Ingy döt Net <ingy@ingy.net>
2014-08-16 15:43:55 -0700

    CPAN Release 0.07
    - Remove 'use 5.x.x' from code

--- lib/YamlTime/Git.pm
+++ lib/YamlTime/Git.pm
@@ -1,9 +1,8 @@
-use 5.008003;
-use YamlTime 0.10 ();
-
+use strict; use warnings;
 package YamlTime::Git;
+our $VERSION = '0.07';
 
-our $VERSION = '0.06';
+use YamlTime 0.10 ();
 
 # XXX Put this here on 28/07/2011. Leave in for a while...
 {
